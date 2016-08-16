7cec854d94213b2629f9a145a534513f1ada43cc
Cody Mercer <me@codymercer.com>
2016-08-16 16:34:59 -0500

    refactor to registerForms function

--- src/index.js
+++ src/index.js
@@ -65,39 +65,39 @@ function FormHelpers (Vue) {
          * The 'beforeCreate' life-cycle hook for Vue 2.0.
          */
         beforeCreate() {
-            let forms = this.$options.forms;
-
-            if (typeof forms == 'object') {
-                let dataIsFunction = typeof this.$options.data == 'function';
-                let data = dataIsFunction ? this.$options.data() : this.$options.data || {};
-
-                for (var form in forms) {
-                    data[form] = forms[form];
-                }
-
-                this.$options.data = dataIsFunction ? function () { return data } : data;
-            }
+            registerForms(this);
         },
 
         /*
          * The 'init' life-cycle hook for Vue 1.0.
          */
         init() {
-            let forms = this.$options.forms;
-
-            if (typeof forms == 'object') {
-                let dataIsFunction = typeof this.$options.data == 'function';
-                let data = dataIsFunction ? this.$options.data() : this.$options.data || {};
-
-                for (var form in forms) {
-                    data[form] = forms[form];
-                }
-
-                this.$options.data = dataIsFunction ? function () { return data } : data;
-            }
+            registerForms(this);
         }
 
     });
+}
+
+/*
+ * Register the forms in the forms option.
+ */
+function registerForms(vm) {
+    let forms = this.$options.forms;
+
+    if (typeof forms == 'object') {
+        let dataIsFunction = typeof vm.$options.data == 'function';
+        let data = dataIsFunction ? vm.$options.data() : vm.$options.data;
+
+        if (typeof data == 'undefined') {
+            data = {};
+        }
+
+        for (var form in forms) {
+            data[form] = forms[form];
+        }
+
+        vm.$options.data = dataIsFunction ? function () { return data } : data;
+    }
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
